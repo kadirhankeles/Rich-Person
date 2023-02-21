1f3acb79b3345de23e2bef33b91236997191d3fb
Kadirhan Keleş <kadirhankeles@gmail.com>
2023-02-21 16:57:46 +0300

    Initial Commit

---
+++ Rich Person/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rich Person
+//
+//  Created by Kadirhan Keles on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
